b5585987264aa728d17b7e7c05711fa1ab116ac9
tom houri <tomhouri@mail.tau.ac.il>
2020-11-14 10:48:36 +0200

    add number supports

--- Caesar/Caesar.c
+++ Caesar/Caesar.c
@@ -11,17 +11,26 @@ char decipher_or_encryption(char letter, enum state op, int key)
 	 int key - key
 	 this function checks that ascii valuse of letter and according to it decids on a pivot
 	 if its a small letter the pivot will be 'a' if its CAPS the pivot will be 'A'
-	 and if it is neither then it must me a special character so it does not change
+	 and if it is a number the pivot will be '0'.
+	 additionaly the mod, for modulo, is 26 for charcter and 10 for digits. 
+	 if it is neither then it must me a special character so it does not change
 	 then the function deciphers or encrypts according to the op and key as seen in the
 	 examples.*/
-	char pivot;
+	char pivot; int mod;
 	if ('a' <= letter && letter <= 'z')
 	{
+		mod = 26;
 		pivot = 'a';
 	}
 	else if ('A' <= letter && letter <= 'Z')
 	{
+		mod = 26;
 		pivot = 'A';
+	}
+	else if ('0' <= letter && letter <= '9')
+	{
+		mod = 10;
+		pivot = '0';
 	}
 	else
 		return letter;
@@ -29,10 +38,10 @@ char decipher_or_encryption(char letter, enum state op, int key)
 	switch (op)
 	{
 	case DECIPHER:
-		return pivot + (letter - pivot - (key%26)+26) % 26;
+		return pivot + (letter - pivot - (key%mod)+mod) % mod;
 			break;
 	case ENCRYPT:
-		return pivot + ((letter - pivot + key) % 26);
+		return pivot + ((letter - pivot + key) % mod);
 			break;
 	default:
 		printf("problem\n");
--- Caesar/main.c
+++ Caesar/main.c
@@ -5,13 +5,10 @@
 int main(int argc, char* argv[])
 {	
 	enum state op = DECIPHER;
-	int key = 1; int line_len;
-	char line[20] = "aBCDEFG";
+	int key = 27; int line_len;
+	char line[50] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+./'`";
 	char* p_line = &line[0];
 	line_len = (signed int)strlen(p_line);
-	int mod;
-	mod = -30 % 26;
-	printf("%d\n", mod);
 	printf("%s\n", p_line);
 	printf("%d\n", line_len);
 	decipher_or_encrypt_line(p_line, op, key);
